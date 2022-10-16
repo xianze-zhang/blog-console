a5b2d4e9464bcef265cea56e444b5e6e539f4b8b
xianze-zhang <zhangxzin@163.com>
2022-10-16 21:03:39 +0800

    feat: 响应处理--分页处理

---
+++ common/page.go
@@ -0,0 +1,36 @@
+package common
+
+import (
+	"blog-console/global"
+	"github.com/gin-gonic/gin"
+)
+
+func GetPage(c *gin.Context) int {
+	page := StrTo(c.Query("page")).MustInt()
+	if page <= 0 {
+		return 1
+	}
+
+	return page
+}
+
+func GetPageSize(c *gin.Context) int {
+	pageSize := StrTo(c.Query("page_size")).MustInt()
+	if pageSize <= 0 {
+		return global.AppSetting.DefaultPageSize
+	}
+	if pageSize > global.AppSetting.MaxPageSize {
+		return global.AppSetting.MaxPageSize
+	}
+
+	return pageSize
+}
+
+func GetPageOffset(page, pageSize int) int {
+	result := 0
+	if page > 0 {
+		result = (page - 1) * pageSize
+	}
+
+	return result
+}
